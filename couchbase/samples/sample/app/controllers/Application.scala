59f5b1176e75e9f7b112e287e135ef0aa625f587
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2013-07-05 15:15:06 +0200

    Add very basic sample

---
+++ couchbase/samples/sample/app/controllers/Application.scala
@@ -0,0 +1,24 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+import play.api.libs.json._
+import play.api.Play.current
+
+import org.ancelin.play2.couchbase.Couchbase
+import play.api.Play.current
+import play.autosource.couchbase.CouchbaseAutoSourceController
+import play.api.libs.concurrent.Execution.Implicits._
+
+case class Person(name: String, surname: String, datatype: String = "person")
+
+object Person {
+  implicit val fmt = Json.format[Person]
+}
+
+object PersonController extends CouchbaseAutoSourceController[Person] {
+  val bucket = Couchbase.bucket("default")
+  override val defaultViewName = "by_name"
+  override val defaultDesignDocname = "person"
+}
---
+++ couchbase/samples/sample/project/Build.scala
@@ -0,0 +1,23 @@
+import sbt._
+import Keys._
+import play.Project._
+
+object ApplicationBuild extends Build {
+
+  val appName         = "sample"
+  val appVersion      = "1.0-SNAPSHOT"
+
+  val mandubianRepo = Seq(
+    "Mandubian repository snapshots" at "https://github.com/mandubian/mandubian-mvn/raw/master/snapshots/",
+    "Mandubian repository releases" at "https://github.com/mandubian/mandubian-mvn/raw/master/releases/"
+  )
+
+  val appDependencies = Seq()
+
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    resolvers ++= mandubianRepo,
+    libraryDependencies ++= Seq(
+      "play-autosource"   %% "couchbase"       % "0.1-SNAPSHOT"
+    )
+  )
+}
