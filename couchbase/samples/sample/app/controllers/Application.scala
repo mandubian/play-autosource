b4b6fc5a8a146fbd71b20f5db08a0c19ccc66d85
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2013-07-10 22:29:27 +0200

    Cleanup sample

--- couchbase/samples/sample/app/controllers/Application.scala
+++ couchbase/samples/sample/app/controllers/Application.scala
@@ -1,11 +1,6 @@
 package controllers
 
-import play.api._
-import play.api.mvc._
-
 import play.api.libs.json._
-import play.api.Play.current
-
 import org.ancelin.play2.couchbase.Couchbase
 import play.api.Play.current
 import play.autosource.couchbase.CouchbaseAutoSourceController
