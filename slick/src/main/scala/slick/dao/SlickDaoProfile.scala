5c09fbbe1ecd4d07ec442c0adcf9766e96606821
Renato Cavalcanti <renato@xavena.eu>
2013-09-11 17:47:28 +0200

    some code formatting

--- slick/src/main/scala/slick/dao/SlickDaoProfile.scala
+++ slick/src/main/scala/slick/dao/SlickDaoProfile.scala
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -22,17 +22,24 @@ import play.api.Play.current
 
 
 trait SlickDao[E <: Entity[E]] {
-  def count() : Int
-  def add(entity: E) : E
-  def update(entity: E) : Unit
+  def count(): Int
+
+  def add(entity: E): E
+
+  def update(entity: E): Unit
+
   def delete(entity: E): Boolean
+
   def deleteById(id: Long): Boolean
+
   def findOptionById(id: Long): Option[E]
+
   def findById(id: Long): E
+
   def pagesList(pageIndex: Int, limit: Int): List[E]
+
   def list(): List[E]
 }
-
 
 
 trait SlickDaoProfile {
@@ -45,17 +52,15 @@ trait SlickDaoProfile {
 
     def id: Column[Long]
 
-
-    def count() : Int = {
-      DB.withSession { implicit sess =>
+    def count(): Int = DB.withSession {
+      implicit sess =>
         Query(this.length).first()
-      }
     }
 
 
     private def addWithAutoInc(entity: E): Long =
       DB.withTransaction {
-        implicit sess: Session =>
+        implicit sess =>
           this.*.returning(this.id).insert(entity)
       }
 
@@ -64,35 +69,32 @@ trait SlickDaoProfile {
       entity.withId(id)
     }
 
-    def update(entity: E) {
-      DB.withTransaction {
-        implicit sess: Session =>
+    def update(entity: E): Unit =
+      DB.withTransaction { implicit sess: Session =>
           entity.id match {
             case Some(id) => filterQuery(id).update(entity)
             case None => throw new CannotUpdateNonPersistedEntityException(entity)
           }
       }
-    }
+
 
     private def filterQuery(id: Long) = this.filter(_.id === id)
 
-    def delete(entity: E): Boolean = {
+    def delete(entity: E): Boolean =
       entity.id match {
         case Some(id) => deleteById(id)
         case None => false
       }
-    }
+
 
     def deleteById(id: Long): Boolean =
-      DB.withTransaction {
-        implicit sess: Session =>
+      DB.withTransaction { implicit sess =>
           filterQuery(id).delete > 0
       }
 
     def findOptionById(id: Long): Option[E] =
-      DB.withTransaction {
-        implicit sess: Session =>
-          this.createFinderBy(_.id).firstOption(id)
+      DB.withTransaction { implicit sess =>
+        this.createFinderBy(_.id).firstOption(id)
       }
 
     def findById(id: Long): E =
@@ -100,21 +102,19 @@ trait SlickDaoProfile {
 
 
     def pagesList(pageIndex: Int, limit: Int): List[E] =
-      DB.withTransaction {
-        implicit sess: Session =>
-          val query = for {entity <- this} yield entity
-          query.list.drop(pageIndex).take(limit)
+      DB.withTransaction { implicit sess =>
+        val query = for {entity <- this} yield entity
+        query.list.drop(pageIndex).take(limit)
       }
 
     def list(): List[E] =
-      DB.withTransaction {
-        implicit sess: Session =>
-          val query = for {entity <- this} yield entity
-          query.list
+      DB.withTransaction { implicit sess =>
+        val query = for {entity <- this} yield entity
+        query.list
       }
 
-
   }
+
 }
 
 
