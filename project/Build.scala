2fc605b1e76c4299c3ce2e6640da1c6af664c881
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2014-02-11 17:03:23 +0100

    Use ReactiveCouchbase

--- project/Build.scala
+++ project/Build.scala
@@ -33,9 +33,9 @@ object ApplicationBuild extends Build {
   ) aggregate(
     core,
     reactivemongo,
-    datomisca/*,
-    couchbase,
-    slick*/
+    datomisca,
+    couchbase
+    /*slick*/
   )
 
   lazy val core = Project(
@@ -89,7 +89,7 @@ object ApplicationBuild extends Build {
       )
     )
   ) dependsOn(core)
-/*
+
   lazy val couchbase = Project(
     id = "couchbase",
     base = file("couchbase"),
@@ -98,19 +98,19 @@ object ApplicationBuild extends Build {
       version := "2.0-SNAPSHOT",
 
       resolvers ++= Seq(
-        "Ancelin Repository" at "https://raw.github.com/mathieuancelin/play2-couchbase/master/repository/snapshots",
-        "Spy Repository" at "http://files.couchbase.com/maven2"
+        "ReactiveCouchbase Snapshots" at "https://raw.github.com/ReactiveCouchbase/repository/master/snapshots/"
       ),
       libraryDependencies ++= Seq(
-        "org.ancelin.play2.couchbase" %% "play2-couchbase"   % "0.5-SNAPSHOT",
-        "com.typesafe.play"           %% "play"              % "2.2.1"        % "provided",
-        "com.typesafe.play"           %% "play-cache"        % "2.2.1"        % "provided"
+        "org.reactivecouchbase" %% "reactivecouchbase-core" % "0.2-SNAPSHOT",
+        "org.reactivecouchbase" %% "reactivecouchbase-play" % "0.2-SNAPSHOT",
+        "com.typesafe.play"     %% "play"                   % "2.2.1"         % "provided",
+        "com.typesafe.play"     %% "play-cache"             % "2.2.0"         % "provided"
       )
     )
   ) dependsOn(core)
 
 
-
+ /*
   lazy val slick = Project(
     id = "slick",
     base = file("slick"),
