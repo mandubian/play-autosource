1acb7c4aed7c06a96d8969676a958e12e235fcb5
Pascal Voitot <pascal.voitot.dev@gmail.com>
2014-06-27 15:40:13 +0200

    migrated to v2.1 with play2.3.1/scala2.10.4 & bintray publish

--- project/Build.scala
+++ project/Build.scala
@@ -1,5 +1,6 @@
 import sbt._
 import Keys._
+import bintray.Plugin.bintraySettings
 
 object ApplicationBuild extends Build {
   val buildName    = "play-autosource"
@@ -9,7 +10,7 @@ object ApplicationBuild extends Build {
   // but can evolve with its own minor version
   // For example:
   // coreVersion = 2.0 -> reactiveMongo v2.0, 2.1, 2.2, 2.3...
-  val coreVersion  = "2.0"
+  val coreVersion  = "2.1"
 
   val mandubianRepo = Seq(
     "Mandubian repository snapshots" at "https://github.com/mandubian/mandubian-mvn/raw/master/snapshots/",
@@ -18,7 +19,7 @@ object ApplicationBuild extends Build {
   )
 
   val BuildSettings = Defaults.defaultSettings ++ Seq(
-    scalaVersion := "2.10.3",
+    scalaVersion := "2.10.4",
     organization := "play-autosource",
 
     resolvers ++= mandubianRepo
@@ -46,8 +47,8 @@ object ApplicationBuild extends Build {
 
       libraryDependencies ++= Seq(
         "play-json-zipper"  %% "play-json-zipper"  % "1.0"                      ,
-        "com.typesafe.play" %% "play-json"         % "2.2.1"                    ,
-        "com.typesafe.play" %% "play"              % "2.2.1"        % "provided",
+        "com.typesafe.play" %% "play-json"         % "2.3.1"                    ,
+        "com.typesafe.play" %% "play"              % "2.3.1"        % "provided",
         "org.specs2"        %% "specs2"            % "1.13"         % "test"    ,
         "junit"              % "junit"             % "4.8"          % "test"
       )
@@ -59,13 +60,13 @@ object ApplicationBuild extends Build {
     base = file("reactivemongo"),
     settings = BuildSettings ++ Seq(
       // can be customized by keeping major version of the core version
-      version := "2.0-SNAPSHOT",
+      version := "2.1-SNAPSHOT",
 
-      resolvers += "Sonatype Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/",
+      resolvers += "Sonatype Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots/",
       libraryDependencies ++= Seq(
-        "org.reactivemongo" %% "play2-reactivemongo" % "0.10.1",
-        "org.reactivemongo" %% "reactivemongo"       % "0.10.0",
-        "com.typesafe.play" %% "play"                % "2.2.1"        % "provided"
+        "org.reactivemongo" %% "play2-reactivemongo" % "0.11.0-SNAPSHOT",
+        "org.reactivemongo" %% "reactivemongo"       % "0.11.0-SNAPSHOT",
+        "com.typesafe.play" %% "play"                % "2.3.1"        % "provided"
       )
     )
   ) dependsOn(core)
@@ -75,17 +76,17 @@ object ApplicationBuild extends Build {
     base = file("datomisca"),
     settings = BuildSettings ++ Seq(
       // can be customized by keeping major version of the core version
-      version := "2.0",
+      version := "2.1",
 
       resolvers ++= Seq(
         "datomisca-mvn-repo" at "http://dl.bintray.com/content/pellucid/maven",
         "clojars"   at "https://clojars.org/repo"
       ),
       libraryDependencies ++= Seq(
-        "com.pellucid"      %% "datomisca"      % "0.7-alpha-7",
+        "com.pellucid"      %% "datomisca"      % "0.7-alpha-10",
         "com.pellucid"      %% "play-datomisca" % "0.7-alpha-3",
-        "com.datomic"        % "datomic-free"   % "0.9.4324" % "provided" exclude("org.slf4j", "slf4j-nop"),
-        "com.typesafe.play" %% "play"           % "2.2.1"    % "provided"
+        "com.datomic"        % "datomic-free"   % "0.9.4766.16" % "provided" exclude("org.slf4j", "slf4j-nop"),
+        "com.typesafe.play" %% "play"           % "2.3.1"    % "provided"
       )
     )
   ) dependsOn(core)
@@ -95,7 +96,7 @@ object ApplicationBuild extends Build {
     base = file("couchbase"),
     settings = BuildSettings ++ Seq(
       // can be customized by keeping major version of the core version
-      version := "2.0-SNAPSHOT",
+      version := "2.1-SNAPSHOT",
 
       resolvers ++= Seq(
         "ReactiveCouchbase Snapshots" at "https://raw.github.com/ReactiveCouchbase/repository/master/snapshots/"
@@ -103,8 +104,8 @@ object ApplicationBuild extends Build {
       libraryDependencies ++= Seq(
         "org.reactivecouchbase" %% "reactivecouchbase-core" % "0.2-SNAPSHOT",
         "org.reactivecouchbase" %% "reactivecouchbase-play" % "0.2-SNAPSHOT",
-        "com.typesafe.play"     %% "play"                   % "2.2.1"         % "provided",
-        "com.typesafe.play"     %% "play-cache"             % "2.2.0"         % "provided"
+        "com.typesafe.play"     %% "play"                   % "2.3.1"         % "provided",
+        "com.typesafe.play"     %% "play-cache"             % "2.3.1"         % "provided"
       )
     )
   ) dependsOn(core)
@@ -114,7 +115,7 @@ object ApplicationBuild extends Build {
     base = file("slick"),
     settings = BuildSettings ++ Seq(
       // can be customized by keeping major version of the core version
-      version := "2.0-SNAPSHOT",
+      version := "2.1-SNAPSHOT",
 
       resolvers ++= Seq(
         "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/",
@@ -124,8 +125,8 @@ object ApplicationBuild extends Build {
 
       libraryDependencies ++= Seq(
         "com.typesafe.play"   %%  "play-slick"    % "0.5.0.4",
-        "com.typesafe.play"   %%  "play"          % "2.2.1"        % "provided",
-        "com.typesafe.play"   %%  "play-jdbc"     % "2.2.1"        % "provided"
+        "com.typesafe.play"   %%  "play"          % "2.3.1"        % "provided",
+        "com.typesafe.play"   %%  "play-jdbc"     % "2.3.1"        % "provided"
       )
     )
   ) dependsOn(core)
@@ -133,12 +134,13 @@ object ApplicationBuild extends Build {
   object Publish {
     lazy val settings = Seq(
       publishMavenStyle := true,
+      licenses += ("Apache-2.0", url("http://www.apache.org/licenses/LICENSE-2.0")),
       publishTo <<= version { (version: String) =>
         val localPublishRepo = "../mandubian-mvn/"
         if(version.trim.endsWith("SNAPSHOT"))
           Some(Resolver.file("snapshots", new File(localPublishRepo + "/snapshots")))
         else Some(Resolver.file("releases", new File(localPublishRepo + "/releases")))
       }
-    )
+    ) ++ bintraySettings
   }
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,3 +1,10 @@
-resolvers += "Sonatype snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/"
+resolvers ++= Seq(
+  "Sonatype snapshots" at "https://oss.sonatype.org/content/repositories/snapshots/",
+  Resolver.url("bintray-sbt-plugin-releases",
+    url("http://dl.bintray.com/content/sbt/sbt-plugin-releases")
+  )(Resolver.ivyStylePatterns)
+)
 
 addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.6.0-SNAPSHOT")
+
+addSbtPlugin("me.lessis" % "bintray-sbt" % "0.1.1")
